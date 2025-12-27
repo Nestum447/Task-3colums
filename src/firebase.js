af9a6a5c97e64c11bc04e4193277db80ce2e7652
Nestum447 <ncarpio935@gmail.com>
2025-12-26 21:24:11 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,17 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5iLXD-lyu6PZ0x0LrmK9khxWqrPPlPcA",
+  authDomain: "gestor-tareas-36fc5.firebaseapp.com",
+  projectId: "gestor-tareas-36fc5",
+  storageBucket: "gestor-tareas-36fc5.firebasestorage.app",
+  messagingSenderId: "326368524776",
+  appId: "1:326368524776:web:531cea029c0afde8b28c5d",
+  measurementId: "G-667RSDXM8Q",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
